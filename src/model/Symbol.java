868958102fb49aec9e111c67049e7ad368443fcb
heroandtn3 <heroandtn3@gmail.com>
2012-12-17 22:00:06 +0700

    Style code in Symbol class

--- src/model/Symbol.java
+++ src/model/Symbol.java
@@ -54,28 +54,28 @@ public enum Symbol {
 	
 	public static int toInt(Symbol sb) {
 		switch (sb) {
-		case EMPTY:
-			return 0;
-		case ONE:
-			return 1;
-		case TWO:
-			return 2;
-		case THREE:
-			return 4;
-		case FOUR:
-			return 4;
-		case FIVE:
-			return 5;
-		case SIX:
-			return 6;
-		case SEVEN:
-			return 7;
-		case EIGHT:
-			return 8;
-		case NINE:
-			return 9;
-		default:
-			return 0;
+			case EMPTY:
+				return 0;
+			case ONE:
+				return 1;
+			case TWO:
+				return 2;
+			case THREE:
+				return 3;
+			case FOUR:
+				return 4;
+			case FIVE:
+				return 5;
+			case SIX:
+				return 6;
+			case SEVEN:
+				return 7;
+			case EIGHT:
+				return 8;
+			case NINE:
+				return 9;
+			default:
+				return 0;
 		}
 	}
 }
